d5af3647cd73a234d3c7e3f4b3a2a9e82b4e140a
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-10-31 02:10:09 +0600

    Pattern Program

---
+++ hollow_rectangle_pattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int row, column;
+    cin>>row>>column; 
+    cout<<"Number of row & column is:"<<row<<" "<<column<<endl;
+
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=column; j++){
+            if(i==1 || i==row){
+                cout<<"* ";
+            }else if(j==1 || j==column){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
---
+++ rectangle_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int row, column;
+    cin>>row>>column; 
+    cout<<"Number of row & column is:"<<row<<" "<<column<<endl;
+
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=column; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
