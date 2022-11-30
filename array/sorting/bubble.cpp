4d91fc4a7831279d41ffee038ae216bb984583a2
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-11-30 23:25:26 +0600

    bubble sort

--- array/sorting/bubble.cpp
+++ array/sorting/bubble.cpp
@@ -14,14 +14,16 @@ int main(){
         cin>>arr[i];
     }
 
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[i]){
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
+    int counter = 1;
+    while(counter<n){
+        for(int i=0; i<n-counter; i++){
+            if(arr[i]>arr[i+1]){
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
             }
         }
+        counter++;
     }
 
     for(int i=0; i<n; i++){
